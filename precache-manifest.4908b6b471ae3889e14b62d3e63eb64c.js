9cf77c3de60a99b5b91230fe44d915417a23fc96
LiorZana <liorzana@gmail.com>
2020-09-07 17:22:23 +0300

    Updates

---
+++ precache-manifest.4908b6b471ae3889e14b62d3e63eb64c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bde34559608d3ada5dd23b7a6114132",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "93677e23dc9bcdd30799",
+    "url": "/pokedex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bd4776ade9c2310f430e",
+    "url": "/pokedex/static/css/main.59c6338e.chunk.css"
+  },
+  {
+    "revision": "93677e23dc9bcdd30799",
+    "url": "/pokedex/static/js/2.2a2fbb84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.2a2fbb84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4776ade9c2310f430e",
+    "url": "/pokedex/static/js/main.d24593f8.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "d749ab74b876d5c52256c0559b487aff",
+    "url": "/pokedex/static/media/PokemonHollow.d749ab74.woff"
+  }
+]);
